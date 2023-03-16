4411dee22213aa11f5a19fda0482c296c30a5c76
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-03-17 01:41:49 +0200

    save data by redux persist

--- src/ReduxToolKit/store.js
+++ src/ReduxToolKit/store.js
@@ -1,15 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
-import addItemSlice from './slices/addItemSlice'
-import addWishSlice from './slices/addWishSlice'
-import authSlice from './slices/authSlice'
-import totalSlice from './slices/totalSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import addItemSlice from './slices/addItemSlice';
+import addWishSlice from './slices/addWishSlice';
+import authSlice from './slices/authSlice';
+import totalSlice from './slices/totalSlice';
+import { combineReducers } from 'redux';
+// Configure Redux Persist
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export const store = configureStore({
-  reducer: {
-    cart:addItemSlice,
-    wish:addWishSlice,
-    auth:authSlice,
-    getAllTotal:totalSlice,
-  },
-})
+// Create a Redux Persist reducer
+const rootReducer =  combineReducers({
+  cart: addItemSlice,
+  wish: addWishSlice,
+  auth: authSlice,
+  getAllTotal: totalSlice,
+});
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create a Redux store with the persisted reducer
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Create a persisted store using the persistStore function
+const persistedStore = persistStore(store);
+
+export { store, persistedStore };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
-import { Provider } from 'react-redux';
-import {store} from './ReduxToolKit/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistedStore } from "./ReduxToolKit/store";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
     </Provider>
 );
-
 
 reportWebVitals();
